54bdc051b68691ca56cbe1206d58ab99f19a9e86
KOrazgaliyeva <korazgaliyeva@dar.kz>
2020-05-02 16:24:51 +0600

    test

--- src/main/scala/user/Boot.scala
+++ src/main/scala/user/Boot.scala
@@ -11,6 +11,7 @@ import user.secvice.UserEntity
 import user.routes.{ClientActor, ClientRoutes}
 import user.service.PostgresClient
 
+import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContextExecutor, Promise}
 import scala.io.StdIn
 
@@ -30,14 +31,31 @@ object Boot extends App with ClientRoutes {
   val client = PostgresClient()
 
   override val someProps: Props = ClientActor.props(UserEntity.props(client))
+
+//  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+//  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+//  StdIn.readLine() // let it run until user presses return
+//  bindingFuture
+//    .flatMap(_.unbind()) // trigger unbinding from the port
+//    .onComplete(_ => system.terminate()) // and shutdown when done
+
+
+  Http().bindAndHandle(route, "0.0.0.0", 8080)
+  println(s"Server running at http://localhost:8080/")
+  Await.result(system.whenTerminated, Duration.Inf)
+
   system.registerOnTermination(session.close())
   system.registerOnTermination(println("Shutting down Actor System."))
 
-  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-  StdIn.readLine() // let it run until user presses return
-  bindingFuture
-    .flatMap(_.unbind()) // trigger unbinding from the port
-    .onComplete(_ => system.terminate()) // and shutdown when done
+  sys.addShutdownHook {
+
+    try {
+      println("SHUTDOWN HOOK: Closing connection.")
+      session.close()
+    } catch {
+      case e: Throwable =>
+        println("Error while leaving cluster: ex=" + e.getMessage)
+    }
+  }
 
 }
